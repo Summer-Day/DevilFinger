de50956d56d3a2df0e04a6c776518adc677e5b33
wangshuo <wangshuofatih@163.com>
2016-07-07 20:38:23 +0800

    添加一个圆形控件和图形文字

--- app/src/main/java/com/example/administrator/devilfinger/ui/fragment/DrawerFragment.java
+++ app/src/main/java/com/example/administrator/devilfinger/ui/fragment/DrawerFragment.java
@@ -1,12 +1,15 @@
 package com.example.administrator.devilfinger.ui.fragment;
 
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.administrator.devilfinger.R;
 import com.example.administrator.devilfinger.activity.MainActivity;
@@ -14,12 +17,16 @@ import com.example.administrator.devilfinger.model.Category;
 import com.example.administrator.devilfinger.ui.adapter.DrawerAdapter;
 import com.example.administrator.wangshuobaselib.BaseFragment;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 
 /**
- * A simple {@link android.support.v4.app.Fragment} subclass.
- *
+ * A simple {@link Fragment} subclass.
  */
 public class DrawerFragment extends BaseFragment {
+    @InjectView(R.id.like)
+    TextView like;
     private ListView mListView;
 
     private DrawerAdapter mAdapter;
@@ -34,6 +41,18 @@ public class DrawerFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mActivity = (MainActivity) getActivity();
+
+        try
+        {
+                Typeface iconfont = Typeface.createFromFile("src/main/iconfont/iconfont.ttf");   //(DFApplication.getContext().getAssets(), "src/main/iconfont/iconfont.ttf");
+            like.setTypeface(iconfont);
+            like.setTextSize(40);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
         View contentView = inflater.inflate(R.layout.fragment_drawer, null);
         mListView = (ListView) contentView.findViewById(R.id.listView);
         mAdapter = new DrawerAdapter(mListView);
@@ -46,6 +65,13 @@ public class DrawerFragment extends BaseFragment {
                 mActivity.setCategory(Category.values()[position]);
             }
         });
+        ButterKnife.inject(this, contentView);
         return contentView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.reset(this);
     }
 }
