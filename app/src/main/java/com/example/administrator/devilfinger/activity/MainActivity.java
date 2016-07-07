e6e721e338fa057b25b5bf39945eb1de42c82cee
wangshuo <wangshuofaith@163.com>
2016-07-08 00:51:49 +0800

    test commit in home

--- app/src/main/java/com/example/administrator/devilfinger/activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/devilfinger/activity/MainActivity.java
@@ -25,6 +25,7 @@ import butterknife.InjectView;
 public class MainActivity extends BaseActivity {
 
 
+    //test
     @InjectView(R.id.content_frame)
     FrameLayout contentLayout;
     @InjectView(R.id.blur_image)
