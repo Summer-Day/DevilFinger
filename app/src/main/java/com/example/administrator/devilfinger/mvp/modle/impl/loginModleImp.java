12c310557ad786ba10102235612079364d6e6202
wangshuo <wangshuofatih@163.com>
2016-08-05 17:23:41 +0800

    提交一个okhttp服务器测试方法

--- app/src/main/java/com/example/administrator/devilfinger/mvp/modle/impl/loginModleImp.java
+++ app/src/main/java/com/example/administrator/devilfinger/mvp/modle/impl/loginModleImp.java
@@ -2,6 +2,10 @@ package com.example.administrator.devilfinger.mvp.modle.impl;
 
 import com.example.administrator.devilfinger.mvp.callback.LoginCallBack;
 import com.example.administrator.devilfinger.mvp.modle.LoginModle;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+
+import okhttp3.Call;
 
 /**
  *
@@ -12,28 +16,28 @@ import com.example.administrator.devilfinger.mvp.modle.LoginModle;
 public class LoginModleImp implements LoginModle {
     @Override
     public void checkLogin(String name, String password, LoginCallBack loginCallBack) {
-        if(login(name , password))
-        {
-            loginCallBack.onSuccess("Y");
-        }
-        else
-        {
-            loginCallBack.onFail("N");
-        }
+
+        String url = "http://121.42.171.113:8080/appServer/im/crm/checkLogin";
+        OkHttpUtils
+                .get()
+                .url(url)
+                .addParams("userId", name)
+                .addParams("pwd", password)
+                .build()
+                .execute(new StringCallback()
+                {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+                        System.out.print(""+e);
+                    }
+
+                    @Override
+                    public void onResponse(String response, int id) {
+                        loginCallBack.onSuccess(response);
+                    }
+                });
     }
 
-    private Boolean login(String name, String password) {
-
-        if("Y".equals(name))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-
-    }
 
 
 }
